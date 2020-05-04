28188c2428684e925d04e57a9492729480f254b3
mushuyun <mushuyun@yahoo.com>
2020-05-04 15:10:27 -0400

    adding url

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+var MONGODB_URI = process.env.MONGODB_URI;
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
